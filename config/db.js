c67e26ec9be1f2718522dcd581634557beb4a407
anagha <anaghabj001@gmail.com>
2024-11-28 16:37:53 +0530

    project Initialize

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const env = require('dotenv').config();
+
+const connectDB = async()=>{
+    try{
+
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("db connected")
+    }catch(error){
+        console.log("db not connected")
+        process.exit(1)
+
+    }
+}
+
+module.exports=connectDB;
